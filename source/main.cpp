9ba287f1e205443251a92cc5ab60e4b7b39b0957
Den-Git-CPP <madim-neodart@rambler.ru>
2022-06-03 00:48:40 +0300

    Считываем команды

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
+#include <sstream>
+#include <vector>
 
-int main() {
-std::cout<<"HelloWorld";
-	return 0;
+int main(int argc, char** argv)
+{
+	std::stringstream num_arg;
+	int num_commands {0};
+	std:: vector<std::string> v_all_commands;
+
+	if (argc <= 1) {
+		num_arg << "Incorrect number of arguments";
+	}
+	else
+	{
+		num_commands = std::stoi(argv[1]);
+		std::string line_commands; 
+
+		for(int i=0; i < num_commands;i++){
+		std::getline(std::cin, line_commands);
+		v_all_commands.emplace_back(std::move(line_commands));
+		}
+
+		std::cout <<"bulk:";
+		for(auto elem:v_all_commands){
+			
+			std::cout << elem<<" ";
+			};
+	}
+	
+		
+
+	std::cin.get();
 }
