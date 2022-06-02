273c1147e9555b6b28dd627d1838b9f054822c2d
Den-Git-CPP <madim-neodart@rambler.ru>
2022-06-03 02:06:35 +0300

    Считываем команды до EOF

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,18 @@
 ﻿#include <iostream>
 #include <sstream>
 #include <vector>
+void print_bulk(const std::vector<std::string>& v_all_commands) {
+	std::cout << "bulk:";
+	for (auto elem : v_all_commands) {
+		std::cout << elem << " ";
+	};
+	std::cout << '\n';
+};
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	std::stringstream num_arg;
 	int num_commands {0};
-	std:: vector<std::string> v_all_commands;
+	std::vector<std::string> v_all_commands;
 
 	if (argc <= 1) {
 		num_arg << "Incorrect number of arguments";
@@ -14,21 +20,22 @@ int main(int argc, char** argv)
 	else
 	{
 		num_commands = std::stoi(argv[1]);
-		std::string line_commands; 
+		std::string line_commands;
+		while (!std::cin.eof() && line_commands != "EOF") {
+			for (int i = 0; i < num_commands; i++) {
+				std::getline(std::cin, line_commands);
+				if (!std::cin.eof() && line_commands != "EOF") {
+					v_all_commands.emplace_back(std::move(line_commands));
+				}
+				else {
+					break;
+				};
+			};
 
-		for(int i=0; i < num_commands;i++){
-		std::getline(std::cin, line_commands);
-		v_all_commands.emplace_back(std::move(line_commands));
+			print_bulk(v_all_commands);
+			v_all_commands.clear();
 		}
 
-		std::cout <<"bulk:";
-		for(auto elem:v_all_commands){
-			
-			std::cout << elem<<" ";
-			};
 	}
-	
-		
-
 	std::cin.get();
 }
